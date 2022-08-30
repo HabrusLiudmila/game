dae2ae6fc753f423b136adb46d827606296ed3d7
HabrusLiudmila <odushko1@rambler.ru>
2022-08-30 18:21:12 +0300

    Update script.js

--- guess the number game/script.js
+++ guess the number game/script.js
@@ -8,23 +8,32 @@ function getPlayerName() {
     return playerName
 }
 
-const name = getPlayerName()
+const playerName = getPlayerName()
 
 const getRandomNumber = (maxValue)=> {
     const randomNumber = Math.round(Math.random() * maxValue)
-    return randomNumber
+    return randomNumber;
 }
 
 const randomNumber = getRandomNumber(MAX_VALUE)
 
 const game = ( minValue, maxValue,attNumber, playerName ,randomNumber) =>{
-    alert(`Привет, ${playerName} !
+    alert(`Привет, ${playerName}!
     Я загадал число в интервале от ${minValue} до ${maxValue}. У тебя есть ${attNumber} попыток отгадать его.`)
-}
+
 
 for (let i = 1; i <= attNumber; i++) {
-   const plaerAnswer = prompt('Какое число я загадал?','')
-    
+   const playerAnswer = prompt('Какое число я загадал?','');
+    if(playerAnswer>randomNumber){
+        alert(`Моё число меньше. Попробуй еще. Осталось ${attNumber -i}`)
+    } else if (playerAnswer < randomNumber){
+        alert(`Моё число больше. Попробуй еще. Осталось ${attNumber -i}`)
+    } else {
+        alert(`Это победа! Ты угадал с ${i} попыток`)
+        return
+    }  
+}
+alert(`Задача провалена.`)
 }
 
-/*1:46:10
+game(MIN_VALUE, MAX_VALUE, ATTEMT, playerName, randomNumber)
